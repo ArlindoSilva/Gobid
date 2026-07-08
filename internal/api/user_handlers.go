4ee7e72e2a10afe856762b6e12494801b57f9b7b
ArlindoSilva <arlindoas2@gmail.com>
2026-07-08 10:00:48 -0300

    feat: Cria o service para interagir com o banco e sobe o servidor
    Cria um service para gerenciar usuários no banco de dados, incluindo
    criação e validação de usuários. Gera um hash para senhas e trata erros
    de duplicidade.

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,12 +1,43 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/ArlindoSilva/gobid/internal/jsonutils"
+	"github.com/ArlindoSilva/gobid/internal/services"
+	"github.com/ArlindoSilva/gobid/internal/usecase/user"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	panic("TODO not implemented")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(r.Context(),
+		data.UserName, data.Email, data.Password, data.Bio)
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicatedUsernameOrEmail) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or username already exists",
+			})
+			return
+		}
+
+		_ = jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "could not create user",
+		})
+		return
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
 }
 
-func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request)  {
 	panic("TODO not implemented")
 }
 
---
+++ internal/services/users_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ArlindoSilva/gobid/internal/store/pgstore"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrDuplicatedUsernameOrEmail = errors.New("username or email already exists")
+
+type UserService struct{
+	pool *pgxpool.Pool
+	queries *pgstore.Queries
+}
+
+func NewUserService(pool *pgxpool.Pool) UserService {
+	return UserService{
+		pool: pool,
+		queries: pgstore.New(pool),
+	}
+}
+
+func (us *UserService) CreateUser(ctx context.Context, username, email, password, bio string) (uuid.UUID, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	args:= pgstore.CreateUserParams{
+		UserName: username,
+		Email: email,
+		PasswordHash: hash,
+		Bio: bio,
+	}
+	createdID, err := us.queries.CreateUser(ctx, args)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return uuid.UUID{}, ErrDuplicatedUsernameOrEmail
+		}
+		return uuid.UUID{}, err
+	}
+	return createdID, nil
+}
