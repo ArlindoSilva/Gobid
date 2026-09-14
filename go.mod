a365b782cad1c174aa953387e3a93b330eb94229
ArlindoSilva <arlindoas2@gmail.com>
2026-09-14 17:36:31 -0300

    Finalização do projeto

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 require (
 	github.com/gorilla/csrf v1.7.3 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	golang.org/x/sync v0.21.0 // indirect
 )
--- internal/api/product_handlers.go
+++ internal/api/product_handlers.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/ArlindoSilva/gobid/internal/jsonutils"
+	"github.com/ArlindoSilva/gobid/internal/services"
 	"github.com/ArlindoSilva/gobid/internal/usecase/product"
 	"github.com/google/uuid"
 )
@@ -23,7 +25,7 @@ func (api *Api) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	
-	id, err := api.ProductService.CreateProduct(
+	productId, err := api.ProductService.CreateProduct(
 		r.Context(), userID, data.ProductName, data.Description, data.Baseprice, data.AuctionEnd,
 	)
 	if err != nil {
@@ -33,8 +35,18 @@ func (api *Api) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	ctx, _ := context.WithDeadline(context.Background(), data.AuctionEnd)
+
+	auctionRoom := services.NewAuctionRoom(ctx, productId, api.BidsService )
+
+	 go auctionRoom.Run()
+
+	api.AuctionLobby.Lock()
+	api.AuctionLobby.Rooms[productId] = auctionRoom
+	api.AuctionLobby.Unlock()
+
 	jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
-		"message": "product created with success",
-		"product_id": id,
+		"message": "auction has started with success",
+		"product_id": productId,
 	})
 }
